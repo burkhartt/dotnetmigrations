bbf1aea58170bc8f61bf7d6f9f7af9e2e4b9326b
Tim Burkhart <burkhartt@digitalev.com>
2011-07-26 16:20:01 -0500

    Create a database long value retriever, copying the logic from their current system.

---
+++ src/DotNetMigrations_UnitTests/Tim/LongValueFromDatabaseRetriever.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Common;
+using DotNetMigrations.Core;
+using DotNetMigrations.Core.Data;
+
+namespace DotNetMigrations.UnitTests.Tim
+{
+    public class LongValueFromDatabaseRetriever : ILongValueFromDatabaseRetriever
+    {
+        private readonly DataAccess dataAccess;
+        private readonly ILogger logger;
+
+        public LongValueFromDatabaseRetriever(DataAccess dataAccess, ILogger logger)
+        {
+            this.dataAccess = dataAccess;
+            this.logger = logger;
+        }
+
+        public long GetLongFromSqlStatement(string sqlStatement)
+        {
+            long currentVersion = -1;
+
+            try
+            {
+                using (var cmd = dataAccess.CreateCommand())
+                {
+                    cmd.CommandText = sqlStatement;
+                    var version = cmd.ExecuteScalar<string>();
+                    if (version != null)
+                    {
+                        version = version.Trim();
+                    }
+                    long.TryParse(version, out currentVersion);
+
+                    if (currentVersion < 0)
+                    {
+                        throw new SchemaException("schema_migrations table appears to be corrupted");
+                    }
+                }
+            }
+            catch (DbException ex)
+            {
+                logger.WriteError(ex.Message);
+            }
+
+            return currentVersion;
+        }
+    }
+}
